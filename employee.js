aca068c92a450b54dc92ec4e5ff6110c48dc2236
Piyush Shaw <piyushshaw8777@gmail.com>
2021-07-14 02:15:33 +0530

    added : welcome message is added

---
+++ employee.js
@@ -0,0 +1,8 @@
+/*************************************************************
+ * Purpose - Implementing employee wage program In javascript
+ * 
+ * @author Rekha
+ * @since 13-07-2021
+ **************************************************************/
+
+console.log("Welcome to Java Script EmployeeWage");
