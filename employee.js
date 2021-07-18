15250e55e221cae2a4cf6136687923dd963bb0f4
Piyush Shaw <piyushshaw8777@gmail.com>
2021-07-18 10:06:12 +0530

    added: store day hour worked and wage earned in a single object

--- employee.js
+++ employee.js
@@ -220,7 +220,7 @@ function totalWages(totalWage, dailyWage){
 	return totalWage + dailyWage;
 }
 console.log("UC8 - Emp Wage Map totalHrs : " + Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
-*/
+
 //UC9:Arrow Function
 
 const IS_PART_TIME = 1;
@@ -294,3 +294,54 @@ empDailyHrsMap.forEach((value,key,map) => {
 console.log("Full Working Days: " +fullWorkingDays);
 console.log("Part Working Days: " +partWorkingDays);
 console.log("Non Working Days: " +nonWorkingDays);
+*/
+//UC10:Object Operation using Arrow Function
+
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const MAX_HRS_IN_MONTH = 160;
+const NUM_OF_WORKING_DAYS = 20;
+let empHrs = 0;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+
+
+let empDailyHrsAndWageArr = new Array();
+
+function calcDailyWage(empHrs){
+        return empHrs * WAGE_PER_HOUR;
+    }
+
+while (totalEmpHrs <= MAX_HRS_IN_MONTH
+	&& totalWorkingDays < NUM_OF_WORKING_DAYS){
+		totalWorkingDays++;
+		let empCheck = Math.floor(Math.random() * 10) % 3;
+		let empHrs = getWorkingHours(empCheck);
+		totalEmpHrs += empHrs;
+		empDailyHrsAndWageArr.push(
+		{
+			dayNum:totalWorkingDays,
+			dailyHours:empHrs,
+			dailyWage: calcDailyWage(empHrs),
+			toString(){
+			return '\nDay' + this.dayNum + '=> Working Hours = ' + this.dailyHours + ' and Wage Earned = ' +this.dailyWage},
+		});
+	}
+
+function getWorkingHours(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        
+         case IS_FULL_TIME:
+             return FULL_TIME_HOURS;
+              
+         default:
+             return 0;
+		}
+}
+
+console.log("Showing Daily Hours Worked and Wage Earned: " +empDailyHrsAndWageArr);
